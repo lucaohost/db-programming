cf83e9b9842b6eb1237b5144164b671b0d0ad963
Lucas Reginatto de Lima <lucas.reginatto.de.lima@gmail.com>
2018-09-26 00:48:48 -0300

    triggers exercises

--- college/storedProcedures-functions/tables.sql
+++ college/storedProcedures-functions/tables.sql
@@ -1,53 +1,140 @@
-drop database if exists biblioteca;
-create database biblioteca;
-use biblioteca;
+drop database if exists empregos; 
+create database empregos;
+use empregos; 
 
-
-create table cliente(
-	cpf  bigint not null,
-    nome varchar(255),
-    endereco varchar (255),
-    cpf_resposavel bigint not null,
-    bloqueado boolean,
-    primary key (cpf)
+create table empregado (
+    operacao varchar(3) not null,
+    usuario int not null primary key,
+    data date not null,
+    cpf char(12) not null,
+    nome varchar(30) null,
+    data_nascimento date null,
+    endereco varchar(40) null,
+    sexo char(1) null,
+    salario decimal(10,2) null,
+    supervisor char(12) null,
+    horasemprojetos int
 );
 
-create table livro (
-	isbn bigint not null,
-    titulo varchar(255),
-    autor varchar(255),
-    quantidade_atual int,
-    quantiade_total int,
-    primary key (isbn)
+create table projeto (
+    id_projeto int primary key, 
+    nome varchar(64) not null
 );
 
-create table reserva(
-	cpf bigint not null,
-    isbn bigint not null,
-    data date,
-    pendente boolean,
-    constraint fk_clienteReserva foreign key (cpf) references cliente(cpf),
-    constraint fk_livroReserva foreign key (isbn) references livro(isbn)
+create table empregado_projeto (
+    id_usuario int, 
+    id_projeto int,
+    foreign key(id_usuario) references empregado(usuario),
+    foreign key(id_projeto) references projeto(id_projeto)
 );
 
-create table aluga(
-    cpf bigint not null,
-    isbn bigint not null,
-    data_retirada date not null,
-    data_devolucao date,
-    estado enum('retirado','atrasado','devolvido'),
-    constraint fk_clienteAluga foreign key (cpf) references cliente(cpf),
-    constraint fk_livroAluga foreign key (isbn) references livro(isbn),
-    primary key(data_retirada),
-    unique(cpf,isbn,data_retirada)
-);
+insert into empregado values ("insert", 1, current_date(), "411.892.880-95", "maria souza", "1976-10-20", "av. oscar freire são paulo - sp", "feminino", 2900.00, "joão", "20");
+insert into empregado values ("insert", 2, current_date(), "339.169.230-80", "josé da silva", "1969-05-18", "av. paulista são paulo - sp", "masculino", 2450.00, "joão", "40");  
+insert into empregado values ("insert", 3, current_date(), "428.734.220-84", "mariana de andrade", "1985-12-06", "av. tiradentes são paulo - sp", "feminino", 4500.00, "joão", "12");   
 
-create table irregularidade(
-	cpf bigint not null,
-    isbn bigint not null,
-    data_retirada date not null,
-    valor_multa decimal(5,2),
-    constraint fk_clienteIrregularidade foreign key (cpf) references cliente(cpf),
-    constraint fk_livroIrregularidade foreign key (isbn) references livro(isbn),
-    constraint fk_alugaIrregularidade foreign key (data_retirada) references aluga(data_retirada)
-);
+insert into projeto values (1, "projeto graphite");
+insert into projeto values (2, "projeto macassar");
+insert into projeto values (3, "projeto mediware4");
+
+insert into empregado_projeto values (1, 3);
+insert into empregado_projeto values (2, 1);
+insert into empregado_projeto values (2, 2);
+insert into empregado_projeto values (1, 1);
+
+
+delimiter $
+create trigger auditorioupdate after update on empregado
+for each row
+    begin
+        insert into auditoria("update",  current_timestamp(), current_user(), old.id);
+    end; $
+delimiter ;
+
+delimiter $
+create trigger auditoriodelete after delete on empregado
+    for each row 
+    begin
+        insert into auditoria("delete",  current_timestamp(), current_user(), old.id);
+    end $
+delimiter ;
+
+update empregado set data_nascimento = "1976-11-20" where usuario = 1;
+delete from empregado where usuario = 3;
+
+updates).
+
+delimiter $
+create trigger faixa_salarial 
+    before update on empregado
+        for each row 
+            begin
+                if new.salario < 1400,00  then
+                    set new.salario = 1400,00;
+                else if new.salario > 7500,00 then
+                    set new.salario = 7500,00;
+                end if;
+            end; $
+delimiter ;
+
+delimiter $
+create trigger faixa_salarial 
+    before insert on empregado
+        for each row 
+            begin
+                if new.salario < 1400,00  then
+                    set new.salario = 1400,00;
+                else if new.salario > 7500,00 then
+                    set new.salario = 7500,00;
+                end if;
+            end; $
+delimiter ;
+
+insert into empregado values ("insert", 4, current_date(), "131.429.990-51", "rafael bastos", "1978-01-16", "av. 23 de maio são paulo - sp", "masculino", 8000,00, "joão", "05"); 
+update empregado set salario = 800,00 where usuario = 2;
+empregados, emitindo uma mensagem de erro (dica: use o comando signal:
+signal sqlstate '45000' set message_text = 'age less than 0'; )
+
+delimiter $
+create trigger alteracaosalario before update on empregado
+for each row 
+begin
+    if new.salario < old.salario - 50 %
+        set new.salario = old.salario;
+    else if new.salario > old.salario + 50%
+        set new.salario = old.salario; 
+    end if;
+
+signal sqlstate '45000' set message_text = 'não foi possível alterar o salário';
+end $
+delimiter $
+
+update empregado set salario = 2900,00 where usuario = 2;
+
+4) faça um trigger para que um empregado não possa ter mais do que 44 horas na soma dos projetos.
+
+delimiter $
+create trigger tempoemprojetos before update on empregado
+for each row 
+begin
+    if new.horasemprojetos > 44
+        set new.horasemprojetos = old.horasemprojetos;
+    end if;
+    
+signal sqlstate '45000' set message_text = 'não foi possível alterar o tempo em projetos';
+end $
+delimiter $
+
+update empregado set horasemprojetos = 50 where usuario = 1;
+
+delimiter $
+create trigger horariomanip before update on empregado
+for each row begin 
+    if (current_timestamp() < 8:00 or current_timestamp() > 18:00) then
+        set message_text = 'horário permitido para alteração é das 8:00 as 18:00'
+    else if (dayofweek(curdate()) == 1 or dayofweek(curdate()) == 7) then
+        set message_text = 'permitido apenas durante dias úteis'
+    end if;
+end $
+delimiter ;
+
+update empregado set salario = 2500,00 where usuario = 4;
